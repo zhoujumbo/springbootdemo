099ec5af88395ec61c12bf5047cb76c908abaf51
zhoujumbo <shenqi9507@163.com>
2018-12-26 00:35:13 +0800

    线程###相关基础总结

--- src/main/java/com/demo/thread/demo1/DeadlockRisk.java
+++ src/main/java/com/demo/thread/demo1/DeadlockRisk.java
@@ -5,6 +5,34 @@ package com.demo.thread.demo1;
  */
 public class DeadlockRisk {
 
+    private static class Resource{
+        public int value;
+    }
 
+    private Resource resourceA = new Resource();
+    private Resource resourceB = new Resource();
+
+    public int read(){
+        System.out.println("read1");
+        synchronized (resourceA){
+            System.out.println("read2");
+            synchronized (resourceB){
+                System.out.println("read3");
+                return resourceB.value + resourceA.value;
+            }
+        }
+    }
+
+    public void write(int a, int b){
+        System.out.println("write1");
+        synchronized (resourceB){
+            System.out.println("write2");
+            synchronized (resourceA){
+                System.out.println("write3");
+                resourceA.value = a;
+                resourceB.value = b;
+            }
+        }
+    }
 
 }
---
+++ src/main/java/com/demo/thread/demo1/NotifyDemo.java
@@ -0,0 +1,41 @@
+package com.demo.thread.demo1;
+
+import org.junit.jupiter.api.Test;
+
+public class NotifyDemo {
+
+    @Test
+    public void test(){
+
+        ThreadB b = new ThreadB();
+
+        b.start();
+
+        // 取得b的锁
+        synchronized (b){
+            try{
+                System.out.println("等待B完成计算。。。");
+                // 放弃b的锁
+                b.wait();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+            System.out.println("b计算结果："+b.total);
+        }
+    }
+}
+
+class ThreadB extends Thread{
+    int total;
+
+    public void run(){
+        // 拿到自己的锁 b
+        synchronized (this){
+            for(int i=0;i<101;i++){
+                total+=i;
+            }
+            // 计算完成 唤醒 只有该同步结束后才会放弃b锁
+            notify();
+        }
+    }
+}
